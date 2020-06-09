4109bd27cec0167a2e03c9f2c2c88ee1e1c8bae4
sunjungkim <sunjung@adop.co.kr>
2020-06-09 12:27:13 +0900

    no message

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
 require('./bootstrap');
+require('./jquery-3.2.1.min');
+require('./jquery.classyloader.min');
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles(['resources/css/base.css','resources/css/layout.css','resources/css/jquery.mCustomScrollbar.css','resources/css/total_Style.css',],'public/css/all.css')
+    .copyDirectory('resources/images', 'public/images')
+    .copyDirectory('resources/font', 'public/font');
+
+
