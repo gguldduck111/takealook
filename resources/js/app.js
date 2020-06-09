33569db1b99b1c1493c848363c23e11efce946fa
sunjungkim <sunjung@adop.co.kr>
2020-06-09 18:31:14 +0900

    no message

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,1 @@
 require('./bootstrap');
-require('./jquery-3.2.1.min');
-require('./jquery.classyloader.min');
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .styles(['resources/css/base.css','resources/css/layout.css','resources/css/jquery.mCustomScrollbar.css','resources/css/total_Style.css',],'public/css/all.css')
     .copyDirectory('resources/images', 'public/images')
-    .copyDirectory('resources/font', 'public/font');
+    .copyDirectory('resources/font', 'public/fonts')
+    .copy(['resources/js/jquery-3.2.1.min.js','resources/js/jquery.classyloader.min.js','resources/js/jquery.mCustomScrollbar.concat.min.js'], 'public/js');
 
 
